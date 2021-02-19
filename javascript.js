04df24d2b68abd1e1f82d2fb0783725ff6ea1faa
GOBIKRISHNAS <76651403+GOBIKRISHNAS@users.noreply.github.com>
2021-02-19 16:17:52 +0530

    Create javascript.js

---
+++ javascript.js
@@ -0,0 +1,31 @@
+var form=document.getElementById("form");
+var btn=document.getElementById("myBtn");
+var span=document.getElementsByClassName("close")[0];
+btn.onclick=function() {
+	form.style.display="block";
+}
+
+span.onclick=function() {
+	form.style.display="none";
+}
+
+function validationForm() {
+	var userName=document.getElementsByName("User Name")[0].value;
+	var password=document.getElementsByName("Password")[0].value;
+	if (userName=="" || password=="") {
+		alert("Please Type the User Name and Password Correctly...");
+	}
+}
+
+function directPage() {
+	var userName=document.getElementsByName("User Name")[0].value;
+	var password=document.getElementsByName("Password")[0].value;
+	if ((userName!="" || password!="")) {
+		if (userName=="admin" && password=="admin") {
+			window.location="https://gobikrishnas.github.io/images/library.jpg";
+		}
+		else {
+			window.location="https://gobikrishnas.github.io/html-css/library-management/";
+		}
+	}
+}
