ef0cede12b4745cfbf8e7f2faf6e82a29ad9606a
GOBIKRISHNAS <76651403+GOBIKRISHNAS@users.noreply.github.com>
2021-02-22 17:32:07 +0530

    Update javascript.js

--- user-page/javascripts/javascript.js
+++ user-page/javascripts/javascript.js
@@ -1,6 +1,6 @@
-var books_available = 250
-var books_borrwed = 100
-var new_arrivals = 50
+var books_available = 4
+var books_borrwed = 5
+var new_arrivals = 2
 
 document.getElementsByClassName("user-name")[0].innerHTML = localStorage.getItem("userName");
 document.getElementById("new-arrivals").innerHTML = new_arrivals;
