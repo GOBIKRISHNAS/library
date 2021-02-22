327be9df8edf4eda96fadf1e1844b616054ba381
GOBIKRISHNAS <76651403+GOBIKRISHNAS@users.noreply.github.com>
2021-02-22 20:56:12 +0530

    Create javascript.js

---
+++ books-borrowed/javascripts/javascript.js
@@ -0,0 +1,23 @@
+books_list = []
+
+str=""
+
+if (books_list.length>0){
+for (i = 0; i < books_list.length; i++) {
+    str = str + "<li><img src = '"+ books_list[i].img_url + "' alt='' width=180px id='pic'><div class='details'><h1>" + books_list[i].name + "</h1><p>" + books_list[i].author +"</p><p>" + books_list[i].cost + "</p><p>Books Available: " + books_list[i].books_available + "</p><input type='button' value='Buy Now'></div></li>"
+}}
+else{
+    str = "NO Books Available"
+}
+
+document.getElementById("content").innerHTML = str
+
+function validationForm() {
+    
+    if (localStorage.getItem("userName") == "admin") {
+        window.location = "https://gobikrishnas.github.io/library/admin-page/";
+    }
+    else{
+        window.location = "https://gobikrishnas.github.io/library/user-page/";
+    }
+}
