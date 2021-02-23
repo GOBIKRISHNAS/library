cc25f2a47f7d793a89cf46406beb93bb9b01e283
GOBIKRISHNAS <76651403+GOBIKRISHNAS@users.noreply.github.com>
2021-02-23 11:15:55 +0530

    Update javascript.js

--- admin-page/javascripts/javascript.js
+++ admin-page/javascripts/javascript.js
@@ -6,3 +6,9 @@ document.getElementsByClassName("user-name")[0].innerHTML = localStorage.getItem
 document.getElementById("new-arrivals").innerHTML = new_arrivals;
 document.getElementById("books-available").innerHTML = books_available;
 document.getElementById("books-borrowed").innerHTML = books_borrwed;
+
+function search() {
+    var search = document.getElementsByName("search")[0].value;
+    sessionStorage.setItem("search_value", search);
+    location.href = "https://gobikrishnas.github.io/library/search-results/";
+}
